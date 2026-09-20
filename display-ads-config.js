006f313f824280d7ad1c82eadc6c939cc6eb23c1
Keikei-p <jyhokei0124@gmail.com>
2026-09-20 15:48:21 +0900

    Add display ads configuration

---
+++ display-ads-config.js
@@ -0,0 +1,14 @@
+/* つうしんコンパス - ディスプレイ広告設定
+ *
+ * AdSense審査前は enabled=false のままにする。
+ * 承認後に client と広告ユニットIDを設定すると、指定位置へ広告を表示する。
+ */
+window.TC_DISPLAY_ADS = {
+  enabled: false,
+  provider: "adsense",
+  client: "",
+  slots: {
+    content: "",
+    serviceFallback: ""
+  }
+};
